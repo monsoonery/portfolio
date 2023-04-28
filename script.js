576113ca12d4409f71490280d44488656b71ec83
monsoonery <84200320+monsoonery@users.noreply.github.com>
2023-04-28 22:09:29 +0200

    decade!!

--- script.js
+++ script.js
@@ -13,70 +13,71 @@ function w3_close() {
 //TODO:
 // change class and function names
 
-let postsData = "";
+let projectsData = "";
 let currentFilters = {
     labels: [],
     tab: ["Featured"],
     sort: "a-z"
 };
 
-const postsContainer = document.getElementById("posts-container");
-const labelsContainer = document.getElementById("post-labels");
-const tabsContainer = document.getElementById("post-tab");
-const postCount = document.getElementById("post-count");
-const noResults = document.getElementById("no-posts");
+const projectsContainer = document.getElementById("projects-container");
+const labelsContainer = document.getElementById("project-labels");
+const tabsContainer = document.getElementById("project-tabs");
+const filtersContainer = document.getElementById("sorting-filtering-options");
+const projectCount = document.getElementById("project-count");
 
 // this is where the magic happens
-fetch("https://raw.githubusercontent.com/monsoonery/portfolio/f4abb3494668ca6ad088c8f2fc6f387dc858fe08/data.json")
+fetch("https://raw.githubusercontent.com/monsoonery/portfolio/8e336c823c3af611494fd1ff4395b06d6300d571/data.json")
     .then(async (response) => {
         if (!response.ok) {
             throw new Error(`HTTP error: ${response.status}`);
         }
-        postsData = await response.json();
+        projectsData = await response.json();
 
-        // maak een html elementje voor elke post
-        postsData.map((post) => createPost(post));
-        // totaal aantal posts die we hebben
-        postCount.innerText = postsData.length;
+        // totaal aantal projects die we hebben
+        projectCount.innerText = projectsData.length;
 
-        // voor elk overview filter een knopje maken
+        // maak een html elementje voor elk project
+        projectsData.map((project) => createProject(project));
+
+        // voor elke tab een knopje maken
         tabData = ["Featured", "All", "Personal", "Commission", "Work"]
-        tabData.map((tab) => createTabButton("tab", tab, tabsContainer)
-        );
+        tabData.map((tab) => createTabButton("tab", tab, tabsContainer));
 
-        // scan alle posts om alle mogelijke label tags te verzamelen
+        // scan alle projects om alle mogelijke label tags te verzamelen
         labelsData = Array.from([
             ...new Set(
-                postsData
-                    .map((post) => post.labels)
+                projectsData
+                    .map((project) => project.labels)
                     .reduce((acc, curVal) => acc.concat(curVal), [])
             )
         ]);
         labelsData.sort(); //sorteren op alphabet
         // voor elke categorie een filter knopje maken
-        labelsData.map((label) => createLabelButton("labels", label, labelsContainer)
-        );
-        handleFilterPosts(currentFilters);
+        labelsData.map((label) => createLabelButton("labels", label, labelsContainer));
+
+        // filteren sort de projects (featured en a-z on page load)
+        handleFilterProjects(currentFilters);
     });
 
-/* POST CREATION FUNCTION */
-const createPost = (postData) => {
-    const { title, link, image, status, timeline, labels, tab } = postData;
-    const post = document.createElement("div");
-    post.className = "post";
-    post.innerHTML = `
-    <div class="post-column">
-      <a class="post-preview" href="${link}">
-        <img class="post-image" src="${image}">
+/* project CREATION FUNCTION */
+const createProject = (projectData) => {
+    const { title, link, image, status, timeline, labels, tab } = projectData;
+    const project = document.createElement("div");
+    project.className = "project";
+    project.innerHTML = `
+    <div class="project-column">
+      <a class="project-preview" href="${link}">
+        <img class="project-image" src="${image}">
       </a>
-      <div class="post-content">
-        <p class="post-title">${title}</p>
-        <div class="post-tags">
-          ${labels.map((label) => { return '<span class="post-tag">' + label + "</span>"; }).join("")}
+      <div class="project-content">
+        <p class="project-title">${title}</p>
+        <div class="project-tags">
+          ${labels.map((label) => { return '<span class="project-tag">' + label + "</span>"; }).join("")}
         </div>
       </div>
       </div>`;
-    postsContainer.append(post);
+    projectsContainer.append(project);
 };
 
 /*********** FILTER BUTTON FUNCS **************/
@@ -103,7 +104,7 @@ const handleButtonClickLabel = (e, key, param, container) => {
         button.setAttribute("data-state", "inactive");
         currentFilters[key] = currentFilters[key].filter((item) => item !== param);
     }
-    handleFilterPosts(currentFilters);
+    handleFilterProjects(currentFilters);
 };
 
 /*********** OVERVIEW BUTTON FUNCS **************/
@@ -149,9 +150,9 @@ const resetFilterButtons = (currentButton) => {
         }
     })
 }
-const resetPosts = () => {
-    postsContainer.innerHTML = "";
-    postsData.map((post) => createPost(post));
+const resetProjects = () => {
+    projectsContainer.innerHTML = "";
+    projectsData.map((project) => createProject(project));
 }
 const handleButtonClickTab = (e, key, param, container) => {
     const button = e.target;
@@ -167,65 +168,73 @@ const handleButtonClickTab = (e, key, param, container) => {
         button.setAttribute('data-state', 'active');
         currentFilters[key] = [];
         currentFilters[key].push(param);
-        handleFilterPosts(currentFilters);
+        handleFilterProjects(currentFilters);
     }
 };
 
 function handleSort() {
     currentFilters["sort"] = [];
     currentFilters["sort"].push(this.value);
-    handleFilterPosts(currentFilters);
+    handleFilterProjects(currentFilters);
 }
 document.getElementById("sort-dropdown").onchange = handleSort;
 
-// functie om posts te filteren en vervolgens weer te geven
-const handleFilterPosts = (filters) => {
+// functie om projects te filteren en vervolgens weer te geven
+const handleFilterProjects = (filters) => {
     console.log("zojuist gekozen filters:");
     console.log(filters);
     // nieuwe array maken zodat de originele niet gemutate wordt
-    let filteredPosts = [...postsData];
-    console.log("array with all my posts:")
-    console.log(filteredPosts);
+    let filteredProjects = [...projectsData];
+    console.log("array with all my projects:")
+    console.log(filteredProjects);
 
-    // stap 1: filter posts op basis van featured/all/commission etc
+    console.log("saus")
+    console.log(filters.tab)
+
+    // stap 1: filter projects op basis van featured/all/commission etc
     if (filters.tab.length == 1) {
-        filteredPosts = filteredPosts.filter((post) =>
+        filteredProjects = filteredProjects.filter((project) =>
             filters.tab.some((filter) => {
-                return post.tab.includes(filter);
+                return project.tab.includes(filter);
             })
         );
+        if (filters.tab[0] == "Featured") {
+            filtersContainer.style.display = "none";
+        } else {
+            filtersContainer.style.display = "flex";
+        }
     } else {
         alert("dr gaat iets mis met die overview sort");
     }
 
-    // stap 2: filter posts zodat alleen de posts die alle geselecteerde tags bevatten weergegeven worden
+    // stap 2: filter projects zodat alleen de projects die alle geselecteerde tags bevatten weergegeven worden
     if (filters.labels.length > 0) {
-        filteredPosts = filteredPosts.filter((post) =>
+        filteredProjects = filteredProjects.filter((project) =>
             filters.labels.every((filter) => {
-                return post.labels.includes(filter);
+                return project.labels.includes(filter);
             })
         );
     } else {
         //als geen tags zijn geselecteerd valt er ook niks te sorten :)
     }
 
-    // stap 3: sorteer de posts
+    // stap 3: sorteer de projects
     if (filters.sort == "a-z") {
-        filteredPosts.sort(function (a, b) {
+        filteredProjects.sort(function (a, b) {
             return a.title.toLowerCase().localeCompare(b.title.toLowerCase());
         });
     } else if (filters.sort == "z-a") {
-        filteredPosts.sort(function (a, b) {
+        filteredProjects.sort(function (a, b) {
             return b.title.toLowerCase().localeCompare(a.title.toLowerCase());
         });
     } else if (filters.sort == "relevance") {
         // algoritme momentje
     }
 
-    // clear post container en plaats de gefilterde posts op de pagina
+    // clear project container en plaats de gefilterde projects op de pagina
     console.log("huidige selectie: ");
-    console.log(filteredPosts);
-    postsContainer.innerHTML = "";
-    postCount.innerText = filteredPosts.length;
-    filteredPosts.map((post) => createPost(post));
+    console.log(filteredProjects);
+    projectsContainer.innerHTML = "";
+    projectCount.innerText = filteredProjects.length;
+    filteredProjects.map((project) => createProject(project));
 };
