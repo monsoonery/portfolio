0535a5191386c670a85d5106c8b01520e159ce5a
monsoonery <84200320+monsoonery@users.noreply.github.com>
2023-05-18 22:54:07 +0200

    litore update

--- nav.js
+++ nav.js
@@ -54,3 +54,14 @@ document.write(`
     id="myOverlay"></div>
 </div>
 `);
+
+// functions to open and close sidebar
+function openSidebar() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSidebar() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
--- script.js
+++ script.js
@@ -47,16 +47,6 @@ function alphabeticalAZ(a, b) {
     return a.title.toLowerCase().localeCompare(b.title.toLowerCase());
 }
 
-// functions to open and close sidebar
-function openSidebar() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
-
-function closeSidebar() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
 
 // variables for project data
 let projectsData = "";
