2d7fd4c6d604169bf72fe2f99db11f339c7f9676
monsoonery <84200320+monsoonery@users.noreply.github.com>
2023-05-19 22:38:19 +0200

    fixed navbar

--- nav.js
+++ nav.js
@@ -65,13 +65,13 @@ document.write(`
 
 // functions to open and close sidebar
 function openSidebar() {
-    document.getElementById("mySidebar").style.visibility = "visible";
-    document.getElementById("myOverlay").style.visibility = "visible";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
 }
 
 function closeSidebar() {
-    document.getElementById("mySidebar").style.visibility = "hidden";
-    document.getElementById("myOverlay").style.visibility = "hidden";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
 }
 
 
